a9a51c543564e7745513101cd62940880927dcdb
Millie.Chen <millie.chen.ck@gmail.com>
2021-07-25 17:18:09 +0800

    Try Password Done

--- try_pwd.py
+++ try_pwd.py
@@ -8,10 +8,15 @@
 password = 'a123456'
 i = 3
 while i > 0:
+    i = i - 1
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功')
         break
     else:
-        i = i - 1
-        print('密碼錯誤! 你還有', i, '次機會')
+        print('密碼錯誤!')
+        if i > 0:
+            print('你還有', i, '次機會')
+        else:
+            print('密碼已鎖定!')
+
